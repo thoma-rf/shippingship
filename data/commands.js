d9a22462870a4e8f7dedec8989b70ea08d96dae8
thomaryan <thoma@yahoo.com>
2022-07-09 16:15:17 +0700

    opo jare

---
+++ data/commands.js
@@ -0,0 +1,13 @@
+var comms = {
+    "help": "shows this help",
+    "info": "shows the town info, and available sail routes",
+    "attr": "shows your ship attibutes and cargo",
+    "shop": "enter shop, see item price, then use buy/sell command; ex: buy cinnamon 200",
+    "shipwright": "short: ship; enter shipwright for ship repair or buy repair stocks",
+    "tavern": "enter tavern, allows talk with people with command talk; ex: talk barkeep",
+    "sail": "sail to neighboring island; ex: sail rajamas",
+    "clear": "clear the screen",
+    "exit": "exit game"
+}
+
+exports.comms = comms;
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const util = require('util');
 var term = require('terminal-kit').terminal;
-var ship1 = require('./data.js').ship1;
+var ship1 = require('./data/ship.js').ship1;
+var comms = require('./data/commands.js').comms;
 
 
 // Convert `fs.readFile()` into a function that takes the
@@ -16,6 +17,7 @@ var action = [];
 
 var maindata = {};
 
+
 async function loadScene(path) {
     // console.log('reading ' + path);
     const buf = await readFile(path);
@@ -25,6 +27,7 @@ async function loadScene(path) {
         main_text = maindata.main;
         action = maindata['action'];
         render(true);
+        return;
     } catch (error) {
         console.log('reading :' + error);
     }
@@ -39,74 +42,73 @@ async function main() {
 main();
 
 function render(all) {
-    if(all){
+    if (all) {
         // term.clear();
-        term(`\n${main_text}\n` );
+        term(`\n${main_text}\n`);
     }
     term.green('> ');
     term.inputField(
         function (error, entry) {
-            if (entry == 'exit') {
-                term.green("\nGood bye!\n");
-                process.exit();
-            }
-            else {
-                act = action[entry];
-                if (act == null) {
-                    // console.log('n  >>>' + entry);
-                    term.red('\nno such bullshit\n');
-
-                    term.grabInput(false);
+            act = action[entry];
+            if (act == null) {
+                if (comms[entry]) {
+                    proccCommand(entry);
                     render(false);
-                    // process.exit();
-                } else {
-                    term.grabInput(false);
-                    // console.log(entry);
-                    // console.log(myAct[entry]);
-                    // act();
-                    switch (act.ops) {
-                        case 'term':
-                            term.brightBlue(`\n${act.args}\n`);
-                            render(false);
-                            break;
-                        case 'loadScene':
-                            loadScene(act.args);
-                            break;
-                        case 'openShop':
-                            openShop(act.args,maindata.name);
-                            break;
-                        case 'closeApp':
-                            term.green("\nGood bye!\n");
-                            process.exit();
+                    return;
+                }
+                term.red('\ncommand not available this time\n');
+                term.grabInput(false);
+                render(false);
+                // process.exit();
+            } else {
+                term.grabInput(false);
+                // console.log(entry);
+                // console.log(myAct[entry]);
+                // act();
+                switch (act.ops) {
+                    case 'term':
+                        term.brightBlue(`\n${act.args}\n`);
+                        render(false);
+                        break;
+                    case 'loadScene':
+                        loadScene(act.args);
+                        break;
+                    case 'openShop':
+                        openShop(act.args, maindata.name);
+                        break;
+                    case 'closeApp':
+                        term.green("\nGood bye!\n");
+                        process.exit();
 
-                        default:
-                            break;
-                    }
+                    default:
+                        break;
                 }
             }
+
         });
 }
 
 
 function openShop(items, name) {
-    
+
     term(`\nWelcome to ${name} trade post:\n`);
 
     term('Our goods:\n');
     term.brightCyan("--------------------------\n");
     term.brightCyan("Goods\t\tPrice\n");
     term.brightCyan("--------------------------\n");
-    
-    for(var k in items){
-        term.brightBlue(`${k}\t\t ${items[k]}\n`);   
+
+    for (var k in items) {
+        term(`${k}\t\t ${items[k]}\n`);
     }
     term.brightCyan("--------------------------\n");
+    term("you can ^Gbuy ^W, ^Gsell ^Wor ^Gexitshop\n");
     term.green('> ');
     term.inputField(
         function (error, entry) {
-            if (entry == 'exit') {
+            if (entry == 'exitshop') {
                 term.green("\nGood bye!\n");
-                process.exit();
+                render();
             }
             else {
                 term.green("\nok then!\n");
@@ -114,20 +116,55 @@ function openShop(items, name) {
             }
         }
     );
+}
 
-    // term('\nYour cargo:\n');
-    // term.brightCyan("--------------------------\n");
-    // weight = 0;
-    // for(var k in ship1.cargo){
-    //     term.brightBlue(`${k}\t ${ship1.cargo[k]}\n`);   
-    //     weight+=ship1.cargo[k];
-    // }
-    
-    // term.brightCyan("--------------------------\n");
-    // term.brightCyan(`cargo : ${weight} / `);
-    // term.brightBlue(`${ship1.cargo_max} kg\n`);
+function proccCommand(entry) {
+    switch (entry) {
+        case 'help':
+            showHelp();
+            break;
+        case 'attr':
+            showCargo();
+            break;
+        case 'clear':
+            term.clear();
+            break;
+        case 'exit':
+            term.green("\nGood bye!\n");
+            process.exit();
 
-    
+        default:
+            break;
+    }
+}
+
+function showHelp() {
+    term('\nHere are the commands you can use:\n');
+
+    var temp = [];
+    var idx = 0;
+    for (var k in comms) {
+        temp[idx++] = [k + " ", comms[k]];
+    }
+    term.table(
+        temp,
+        { hasBorder: false }
+    );
 
 }
 
+
+function showCargo() {
+    term('\nYour cargo:\n');
+    term.brightCyan("--------------------------\n");
+    weight = 0;
+    for (var k in ship1.cargo) {
+        term.brightBlue(`${k}\t ${ship1.cargo[k]}\n`);
+        weight += ship1.cargo[k];
+    }
+
+    term.brightCyan("--------------------------\n");
+    term.brightCyan(`cargo : ${weight} / `);
+    term.brightBlue(`${ship1.cargo_max} kg\n`);
+
+}
